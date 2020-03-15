5447ab563b1a72fd42a7b2a6a32444f372ac5d45
Constantine Burovytsky <esfero1505@gmail.com>
2020-03-15 23:51:42 +0200

    5.8. JavaDoc. Code convention[#247113]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,17 +1,61 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculate Класс для вычисления арифметических операций
+ *
+ * @author Constantine Burovytsky (burovytsky@gmail.com)
+ */
 public class Calculator {
+    /**
+     * Main
+     * @param args - arguments
+     */
     public static void main(String[] args) {
+
+        /**
+         * number One for calculations
+         */
         int one = 1;
+
+        /**
+         * number Two for calculations
+         */
         int two = 2;
+
+        /**
+         *
+         * calculating the sum of two numbers
+         */
         int onePlusTwo = one + two;
         System.out.println(onePlusTwo);
+        /**
+         * number Four for calculations
+         */
         int four = 4;
+        /**
+         * number Five for calculations
+         */
         int five = 5;
+        /**
+         * number Six for calculations
+         */
         int six = 6;
+        /**
+         *
+         * division of two numbers
+         */
         int sixDivTwo = six / two;
+        /**
+         *
+         * subtraction of two numbers
+         */
         int fiveMinusTwo = five - two;
+        /**
+         *
+         * multiplication of two numbers
+         */
         int fourTimeTwo = four * two;
+
         System.out.println("SixDivTwo = " + sixDivTwo);
         System.out.println("fiveMinusTwo = " + fiveMinusTwo);
         System.out.println("fourTimeTwo = " + fourTimeTwo);
---
+++ src/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Constantine Burovytsky (burovytsky@gmail.com)
+ * @version 1
+ * @since 0.1
+ */
+package ru.job4j.calculator;
