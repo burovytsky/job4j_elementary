4640fc7e0136b83ef1036dcfc43ff67f2b59bb9c
burovytsky <esfero1505@gmail.com>
2020-03-16 18:21:06 +0200

    5.8. JavaDoc. Code convention[#247113] Исправление

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -11,49 +11,15 @@ public class Calculator {
      * @param args - arguments
      */
     public static void main(String[] args) {
-
-        /**
-         * number One for calculations
-         */
         int one = 1;
-
-        /**
-         * number Two for calculations
-         */
         int two = 2;
-
-        /**
-         *
-         * calculating the sum of two numbers
-         */
         int onePlusTwo = one + two;
         System.out.println(onePlusTwo);
-        /**
-         * number Four for calculations
-         */
         int four = 4;
-        /**
-         * number Five for calculations
-         */
         int five = 5;
-        /**
-         * number Six for calculations
-         */
         int six = 6;
-        /**
-         *
-         * division of two numbers
-         */
         int sixDivTwo = six / two;
-        /**
-         *
-         * subtraction of two numbers
-         */
         int fiveMinusTwo = five - two;
-        /**
-         *
-         * multiplication of two numbers
-         */
         int fourTimeTwo = four * two;
 
         System.out.println("SixDivTwo = " + sixDivTwo);
